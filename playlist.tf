1abf8f743101f82890e9c0b2108003ddfd2cd685
iam-masood <mdmasood015@gmail.com>
2024-08-14 20:16:39 -0400

    second update

--- playlist.tf
+++ playlist.tf
@@ -1,4 +1,4 @@
 resource "spotify_playlist" "playlist1" {
-    name = "My Favorite Songs"
-    tracks = ["51EUKH2M1QGN50iGUlnxmU", "7qzsNpJvOR8attSueN3zqq", "1dZuiEX0EjvQai3N7BlUmT"]
+    name = "Masood Playlist"
+    tracks = ["1dZuiEX0EjvQai3N7BlUmT", "7qzsNpJvOR8attSueN3zqq", "51EUKH2M1QGN50iGUlnxmU"]
 }
